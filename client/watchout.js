8ee660ec1e44772c8006f329f25b31febfaec919
erikdbrown <erikdbrown@gmail.com>
2015-12-04 19:47:02 -0800

    circles are animated, moving in a random position

--- client/watchout.js
+++ client/watchout.js
@@ -10,24 +10,25 @@ var gameStats = {
   highscore: 300
 };
 
-var Player = function (x, y, axis) {
+var Player = function (n) {
 
   this.fill = '#ff6600';
-  this.x = x;
-  this.y = y;
-  // this.axis = axis;
-  this.r = 30;
+  this.r = 10;
+  this.x = Math.random() * gameOptions.width;
+  this.y = Math.random() * gameOptions.height;
+  this.id = n || 0;
+  this.m = 'M' + this.x + ', ' + this.y
+  this.l = 'L' + (Math.random() * gameOptions.width) + ', ' + (Math.random() * gameOptions.height)
 
 }
 
-//Player.prototype methods
-
-var players = [new Player(200, 300), new Player(150, 400), new Player(300, 450)];
+var players = [new Player('hi'), new Player(), new Player()];
 
 var svg = d3.select('.board').append('svg')
             .attr('height', gameOptions.height)
-            .attr('width', gameOptions.width);
-(console.log(svg))
+            .attr('width', gameOptions.width)
+            .style('padding', 30)
+            .style('background-color', '#003366')
 
 
 var currentScore = d3.select('.current').selectAll('span')
@@ -39,16 +40,26 @@ var highScore = d3.select('.highscore').selectAll('span')
                   .text(gameStats.highscore);
 
 var circles = svg.selectAll('circle')
-                 .data(players)
-                 .enter()
-                 .append('circle');
+               .data(players)
+               .enter()
+               .append('circle');
 
 var playerStyles = circles
                    .attr('cx', function(d) { return d.x })
                    .attr('cy', function(d) { return d.y })
                    .attr('r', function(d) { return d.r })
-                   .style('fill', function(d) { return d.fill });
+                   .attr('id', function(d) { return d.id })
+                   .style('fill', function(d) { return d.fill }); 
 
+setInterval(function() {
+  var range = d3.range(players.length).map(function(d) { return { x: Math.random() * gameOptions.width, y: Math.random() * gameOptions.height } });
+  var x = circles
+            .data(range)
+            .transition()
+            .duration(1000)
+            .attr('cx', function(d) { return d.x })
+            .attr('cy', function(d) { return d.y })
+            // .transition(3000)
 
-
+}, 1000)
 
